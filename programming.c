2d0429c76cbc50658afb14079827443adec31ab6
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-10-13 13:05:37 +0600

    decrement checked

--- programming.c
+++ programming.c
@@ -31,13 +31,28 @@
 //     printf("Outside for loop\n");
 //     return 0;
 // }
+// #include<stdio.h>
+// #include<stdbool.h>
+// int main()
+// {
+//     printf("Before for loop\n");
+
+//     for(int a = 10; a < 100; a+=10)
+//     {
+//         printf("inside for loop\n");
+//         printf("value of loop = %d\n", a);
+//     }
+
+//     printf("Outside for loop\n");
+//     return 0;
+// }
 #include<stdio.h>
 #include<stdbool.h>
 int main()
 {
     printf("Before for loop\n");
 
-    for(int a = 10; a < 100; a+=10)
+    for(int a = 100; a >= 10; a-=10)
     {
         printf("inside for loop\n");
         printf("value of loop = %d\n", a);
